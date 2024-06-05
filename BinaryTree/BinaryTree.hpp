f2f623cc2052aac012b0008a538529de1ae63322
Ensar-ThinkPad <ensarcehajic02@gmail.com>
2024-06-05 21:20:47 +0200

    Added BinaryTree

---
+++ BinaryTree/BinaryTree.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include <iostream>
+
+template <typename T>
+class Node{
+public:
+T data;
+Node* left;
+Node* right;
+};
+
+template <typename T>
+class BinaryTree{
+
+
+private:
+
+};
